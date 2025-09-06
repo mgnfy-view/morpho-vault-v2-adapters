504bab55c8066cc2b5dbea9bf69e447bf086dce3
Sahil-Gujrati <sahilgujrati12@gmail.com>
2025-09-06 11:49:58 +0530

    feat: move adapter base contract and interface from 'utils' to 'adapterBase' directory

--- src/adapterBase/AdapterBase.sol
+++ src/adapterBase/AdapterBase.sol
@@ -6,13 +6,18 @@ import { IVaultV2 } from "morpho-vault-v2-1.0.0/src/interfaces/IVaultV2.sol";
 
 import { SafeERC20 } from "@openzeppelin-contracts-5.3.0/token/ERC20/utils/SafeERC20.sol";
 
-import { IAdapterBase } from "@src/utils/IAdapterBase.sol";
+import { IAdapterBase } from "@src/adapterBase/interfaces/IAdapterBase.sol";
+
+import { SanityChecks } from "@src/utils/SanityChecks.sol";
 
 /// @title AdapterBase.
 /// @author mgnfy-view.
-/// @notice Base contract to be used by all adapters.
+/// @notice Base contract that holds utility functionality to be used by all
+/// adapters by means of composition.
 abstract contract AdapterBase is IAdapterBase {
     using SafeERC20 for IERC20;
+    using SanityChecks for address;
+    using SanityChecks for uint256;
 
     /// @dev The factory that deployed this contract.
     address internal immutable i_factory;
@@ -20,13 +25,16 @@ abstract contract AdapterBase is IAdapterBase {
     IVaultV2 internal immutable i_morphoVaultV2;
     /// @dev The asset accepted by the Morpho vault
     address internal immutable i_asset;
-    /// @dev a unique identifier for this adapter.
+    /// @dev A unique identifier for this adapter.
     bytes32 internal immutable i_adapterId;
-    /// @dev Can skim reward tokens allocated to this contract.
+    /// @dev Address that can skim tokens sent to this contract. These tokens may be
+    /// airdrop tokens, reward tokens from Merkl campaigns, etc. The skim recipient
+    /// should have the necessary infrastructure to fairly distribute these tokens
+    /// to the vault depositors.
     address internal s_skimRecipient;
 
     /// @notice Initializes the contract.
-    /// @param _morphoVaultV2 The morpho vault v2 this adapter is attached to.
+    /// @param _morphoVaultV2 The morpho vault v2 instance this adapter is attached to.
     /// @param _initialSkimRecipient The initial skm recipient address.
     constructor(address _morphoVaultV2, address _initialSkimRecipient) {
         i_factory = msg.sender;
@@ -39,45 +47,53 @@ abstract contract AdapterBase is IAdapterBase {
     /// @notice Sets the new skim recipient.
     /// @param _newSkimRecipient The new skim recipient address.
     function setSkimRecipient(address _newSkimRecipient) external {
-        _requireMorphoVaultV2Owner(msg.sender);
+        _requireCallerIsMorphoVaultV2Owner(msg.sender);
+        _newSkimRecipient.requireNotAddressZero();
 
         s_skimRecipient = _newSkimRecipient;
 
         emit SkimRecipientSet(_newSkimRecipient);
     }
 
-    /// @notice Skims the adapter's balance of `token` and sends it to `skimRecipient`.
+    /// @notice Skims the adapter's balance of `_token` and sends it to `skimRecipient`.
     /// This is useful to handle rewards that the adapter has earned.
     /// @param _token The token to skim.
     function skim(address _token) external {
-        _requireSkimRecipient(msg.sender);
+        _requireCallerIsSkimRecipient(msg.sender);
 
         IERC20 token = IERC20(_token);
         uint256 balance = token.balanceOf(address(this));
 
         token.safeTransfer(s_skimRecipient, balance);
+
         emit Skimmed(_token, balance);
     }
 
-    /// @notice Checks if the caller is the vault owner.
+    /// @notice Reverts if the caller is not the vault owner.
     /// @param _caller The caller address.
-    function _requireMorphoVaultV2Owner(address _caller) internal view {
+    function _requireCallerIsMorphoVaultV2Owner(address _caller) internal view {
         if (_caller != i_morphoVaultV2.owner()) revert AdapterBase__NotMorphoVaultV2Owner();
     }
 
-    /// @notice Checks if the caller is the skim recipient.
+    /// @notice Reverts if the caller is not the skim recipient.
     /// @param _caller The caller address.
-    function _requireSkimRecipient(address _caller) internal view {
+    function _requireCallerIsSkimRecipient(address _caller) internal view {
         if (_caller != s_skimRecipient) revert AdapterBase__NotSkimRecipient();
     }
 
-    /// @notice Gets the factory that deployed this vault.
+    /// @notice Reverts if the `_asset` is not the asset accepted by the vault.
+    /// @param _asset The asset address.
+    function _requireIsAcceptedAsset(address _asset) internal view {
+        if (_asset != i_morphoVaultV2.asset()) revert AdapterBase__NotAcceptedAsset();
+    }
+
+    /// @notice Gets the factory address that deployed this vault.
     /// @return The factory address.
     function getFactory() external view returns (address) {
         return i_factory;
     }
 
-    /// @notice Gets the Morpho vault v2 address.
+    /// @notice Gets the Morpho vault v2 address this adapter is attached to.
     /// @return The vault address.
     function getMorphoVaultV2() external view returns (address) {
         return address(i_morphoVaultV2);
--- src/adapterBase/interfaces/IAdapterBase.sol
+++ src/adapterBase/interfaces/IAdapterBase.sol
@@ -1,15 +1,22 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.20;
 
-interface IAdapterBase {
+import { IAdapter as IMorphoVaultV2Adapter } from "morpho-vault-v2-1.0.0/src/interfaces/IAdapter.sol";
+
+/// @title IAdapterBase.
+/// @author mgnfy-view.
+/// @notice Interface to be implemented by all adapters.
+interface IAdapterBase is IMorphoVaultV2Adapter {
     event SkimRecipientSet(address indexed newSkimRecipient);
     event Skimmed(address indexed token, uint256 indexed balance);
 
     error AdapterBase__NotMorphoVaultV2Owner();
     error AdapterBase__NotSkimRecipient();
+    error AdapterBase__NotAcceptedAsset();
 
     function setSkimRecipient(address _newSkimRecipient) external;
     function skim(address _token) external;
+
     function getFactory() external view returns (address);
     function getMorphoVaultV2() external view returns (address);
     function getAsset() external view returns (address);
